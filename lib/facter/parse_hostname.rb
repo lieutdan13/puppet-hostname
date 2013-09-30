a2634cec24eca01e59af84e10bfcf097154fd495
Dan Schaefer <dan@schaeferzone.net>
2013-09-30 19:34:09 -0400

    Finalizing the pretty_hostname

--- lib/facter/parse_hostname.rb
+++ lib/facter/parse_hostname.rb
@@ -2,6 +2,7 @@ require 'facter'
 
 Facter.add(:pretty_hostname) do
   setcode do
-    pretty_hostname = %x{hostname} 
+    host = /(.*)(\.\w{2,}\.\w{2,3})$/.match(%x{hostname --fqdn})[1]
+    pretty_hostname = host.split(/(\W)/).map(&:capitalize).join
   end
 end
